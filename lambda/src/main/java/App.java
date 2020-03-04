8c1f3895af8f789aabaf18bf0c255ccf96f5ab2d
zkj <438101845@qq.com>
2020-03-05 00:10:18 +0800

    [zkj]-course3-finished

--- lambda/src/main/java/App.java
+++ lambda/src/main/java/App.java
@@ -1,5 +1,52 @@
+import java.util.function.Consumer;
+import java.util.function.Function;
+
 public class App {
     public static void main(String[] args) {
+        AppendSymbol appendSymbol = string -> {
+            StringBuilder stringBuilder = new StringBuilder("*");
+            stringBuilder.append(string);
+            stringBuilder.append("*");
+            System.out.println(stringBuilder.toString());
+        };
+        Consumer<String> consumer = string -> {
+            StringBuilder stringBuilder = new StringBuilder("*");
+            stringBuilder.append(string);
+            stringBuilder.append("*");
+            System.out.println(stringBuilder.toString());
+        };
+        CalculatorSquare calculatorSquare = number -> number * number;
+        Function<Integer, Integer> function = e -> e * e;
 
+        appendSymbolByFunctional("加星号", appendSymbol);
+        appendSymbolByConsumer("加星号", consumer);
+        System.out.println(squareByFunctional(2, calculatorSquare));
+        System.out.println(squareByFunction(2, function));
+    }
+
+    @FunctionalInterface
+    interface AppendSymbol {
+        void print(String string);
+    }
+
+    @FunctionalInterface
+    interface CalculatorSquare {
+        int numberSquare(int number);
+    }
+
+    static void appendSymbolByFunctional(String string, AppendSymbol appendSymbol) {
+        appendSymbol.print(string);
+    }
+
+    static void appendSymbolByConsumer(String string, Consumer<String> consumer) {
+        consumer.accept(string);
+    }
+
+    static int squareByFunctional(int number, CalculatorSquare calculatorSquare) {
+        return calculatorSquare.numberSquare(number);
+    }
+
+    static int squareByFunction(int number, Function<Integer, Integer> function) {
+        return function.apply(number);
     }
 }
